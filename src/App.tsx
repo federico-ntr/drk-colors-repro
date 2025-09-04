86b910dcc8b8972615a7fbbcb90223ff359f0974
federico-ntr <federicontr@gmail.com>
2025-09-04 08:55:24 +0200

    working combo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Alert } from "design-react-kit";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "typeface-titillium-web";
+import "typeface-roboto-mono";
+import "typeface-lora";
+import "./scss/bootstrap-italia-custom.scss";
+
+function App() {
+  return <Alert>This is an Alert</Alert>;
+}
+
+export default App;
